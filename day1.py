4942372de8e0e538b7f7d5c4cac5233a0776dbff
shvpn <oukoksa11@gmail.com>
2025-01-19 12:22:42 +0700

    d

--- day1.py
+++ day1.py
@@ -14,7 +14,7 @@ client = TelegramClient(
     session='test',
     api_id=api_id,
     api_hash=api_hash,
-    device_model='IPhone X',     # Custom device name
+    device_model='MY BOT',     # Custom device name
     system_version='16.5',         # Custom system version
     app_version='1.0'                   # Custom app version
 )
@@ -26,19 +26,12 @@ async def auto_reply():
         
         # Avoid replying to yourself or channels
         if sender and not sender.bot and not event.is_channel:
-            reply_message = "Wait 15 minutes because I'm currently unavailable."
+            reply_message = "This is an auto-reply. I'm currently unavailable. Perhaps 15 minutes later I will be available."
             await event.reply(reply_message)
             print(f"Replied to {sender.username or sender.id} with: {reply_message}")
 
     print("Auto-reply is active.")
     await client.run_until_disconnected()
-
-
-def get_list_contact():
-    with client:
-        for contact in client.iter_dialogs():
-            print(contact.name)
-            print(contact.id)
 
 
 
